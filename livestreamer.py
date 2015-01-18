fb389be5b5f260141b2b5db4a1898dde06267c53
jocelynthode <jocelynthode@users.noreply.github.com>
2015-01-18 20:33:58 +0100

    Update livestreamer.py

--- livestreamer.py
+++ livestreamer.py
@@ -1,25 +1,7 @@
 #!/usr/bin/env python3
 
 import subprocess
-import os
 import sys
 
-def tryexec(paths, args):
-    for path in paths:
-        basename = os.path.basename(path)
-        try:
-            os.execlp(path, basename, *args)
-        except FileNotFoundError:
-            pass
-    raise FileNotFoundError
-
-def vlc(arg):
-    tryexec(['vlc', r'C:\Program Files (x86)\VideoLAN\VLC\vlc.exe'], 
-        ['--', arg]
-    )
-
-url = sys.argv[1][15:] 
-
-print (url)
-
+url = sys.argv[1][15:]
 subprocess.call(['/usr/bin/livestreamer', url, 'source'])
